ca21d57244fe38688deb7d9ff4aaee446e129617
bulichova <bulichova84@gmail.com>
2021-05-13 00:05:40 +0300

    request

--- src/index.js
+++ src/index.js
@@ -1,2 +1,36 @@
 import css from "./css/styles.css";
-import "./js/gallery.js"
+import "./js/gallery.js";
+import postTemp from "./templates/post.hbs";
+const postList = document.getElementById("postList");
+const url = "  http://localhost:3000/posts";
+// первогачальный get запрос (нужен только url)
+// then это метод promis
+const request = fetch(url)
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    //   убедились что получили массив данных
+    console.log(data);
+    //  сщздали разметку по шаблону из поученного массива данных
+    const posts = postTemp(data);
+    //  убедились что создали разметку по шаблону
+    console.log(posts);
+    // встраиваем разметку по шаблону в элемент списка html к которому получили доступ выше
+    postList.insertAdjacentHTML("afterbegin", posts);
+  });
+console.log(request);
+
+const options = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify({
+    title: "foto",
+    author: "Ya",
+  }),
+};
+fetch(url, options).then((response) => {
+  console.log(response);
+});
