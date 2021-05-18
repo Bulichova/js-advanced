e81f8bf99907dd577e06bbe22afa339a08bfb5a4
bulichova <bulichova84@gmail.com>
2021-05-18 22:45:17 +0300

    kokteils img

--- src/index.js
+++ src/index.js
@@ -2,24 +2,56 @@ import css from "./css/styles.css";
 import "./js/gallery.js";
 import postTemp from "./templates/post.hbs";
 const postList = document.getElementById("postList");
+const alco = document.getElementById("alco");
+const resultList = document.querySelector(".results");
+console.log(resultList);
+console.log(alco);
+// alco.addEventListener("input",(event)=>{
+// // // смотрим обЪект события
+//   console.log(event);
+// //   // достанем из обЪекта события целевой элемент
+//   console.log(event.target);
+// //   // получим из цулевого элемента введенное значение
+//   console.log(event.target.value);
+// })
+alco.addEventListener("change", (event) => {
+  console.log(event);
+  console.log(event.target);
+  console.log(event.target.value);
+  const req = fetch(
+    `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${event.target.value}`,
+  );
+  req
+    .then((response) => {
+      // console.log(response);
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      return data.drinks;
+    })
+    .then((drinks) => {
+      console.log("массив", drinks);
+      const items = drinks.map((elem) => {
+        console.log(elem);
+        console.log(elem.strDrinkThumb);
+        const item = document.createElement("li");
+        console.log(item);
+        const img = document.createElement("img");
+        console.log(img);
+        img.src = elem.strDrinkThumb;
+        img.alt = "img";
+        item.append(img);
+        return item
+      });
+      resultList.append(...items);
+    });
+});
 const url = "  http://localhost:3000/posts";
 // первогачальный get запрос (нужен только url)
 // then это метод promis
-const request = fetch(url)
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    //   убедились что получили массив данных
-    console.log(data);
-    //  сщздали разметку по шаблону из поученного массива данных
-    const posts = postTemp(data);
-    //  убедились что создали разметку по шаблону
-    console.log(posts);
-    // встраиваем разметку по шаблону в элемент списка html к которому получили доступ выше
-    postList.insertAdjacentHTML("afterbegin", posts);
-  });
-console.log(request);
+
+// console.log(request);
 
 const options = {
   method: "POST",
@@ -32,5 +64,14 @@ const options = {
   }),
 };
 fetch(url, options).then((response) => {
-  console.log(response);
+  // console.log(response);
 });
+
+// создать input, в который пользователь будет вводить значение для поиска
+// получить доступ к  input
+// получить значение из input которое мы должны отправить в запрос
+// сделать запрос
+// получить ответ ( результат в виде массива объектов)
+// сделать шаблон разметки
+// расструсить в шаблон каждый объекю
+// сформированные шаблоны отртсовать в браузере
